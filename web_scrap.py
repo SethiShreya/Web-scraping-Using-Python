c8c257fdf7b8bb7b41cdd38905aaaf23d76f4ba1
shreya <shreyasethi47@gmail.com>
2022-03-11 13:38:00 +0530

    added comments

--- web_scrap.py
+++ web_scrap.py
@@ -2,16 +2,20 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import re
-
+# paste below the url where you want to scrap data
 url= 'https://the-bibliofile.com/2022-historical-fiction-books-new-anticipated/'
+# send a get request to scrap data from the url provided
 page= requests.get(url)
 
+# this will beautify the content of the get request as html tags
 soup= BeautifulSoup(page.content, 'html.parser')
+# these three lines will extract the specified tag and store data in variable
 bookname= soup.findAll(attrs= {'class': 'listtitle'})
 authorsname= soup.findAll(attrs= {'class': 'listmeta'})
 date_stuff= soup.findAll(attrs= {'class': 'listtext'})
 
 data= []
+# this loop will extract all the relevant info from the extracted data and create a csv file to store all the extracted data
 for i in range(22):
     name= bookname[i].text
     authors= authorsname[i].text
